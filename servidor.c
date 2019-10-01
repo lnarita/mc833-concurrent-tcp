92e3e61bfe43252cf81f3e607c6efcd00c02729c
Bernardo do Amaral Teodosio <berteodosio@gmail.com>
2019-09-30 22:44:27 -0300

    after playing with child

--- servidor.c
+++ servidor.c
@@ -148,6 +148,7 @@ void handleClientConnectionOnChildProcess(int connfd, int listenfd, struct socka
     handleClientConnection(connfd, clientInfo);
 
     // fecha a conexão com o cliente, depois que o mesmo se desconectou
+    printf("morrendo\n");
     close(connfd);
     exit(0);
 }
